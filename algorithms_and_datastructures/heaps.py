0aba5e37473b25b57f756be5ccbb7232a3d772a5
Claudio Rotolo <claudio.work69@gmail.com>
2022-07-27 08:58:23 +0200

    fix: heaps file

--- algorithms_and_datastructures/heaps.py
+++ algorithms_and_datastructures/heaps.py
@@ -2,18 +2,23 @@ import heapq
 import random
 
 
-def max_h(nums: list[int]):
+def create_max_h(nums: list[int]):
     inverse = [-n for n in nums]
     heapq.heapify(inverse)
-    return -(heapq.heappop(inverse))
+    return inverse
 
-
-def min_h(nums: list[int]):
+def create_min_h(nums: list[int]):
     heapq.heapify(nums)
-    return heapq.heappop(nums)
+    return nums
 
 
 arr = [random.randint(0, 100) for _ in range(15)]
 
-print(max_h(arr))
-print(min_h(arr))
+max_heap = create_max_h(nums)
+print(max_heap)
+
+min_heap = create_min_h(nums)
+print(min_heap)
+
+print("Max in max_heap: " + -(heapq.heappop(inverse)))
+print("Min in min_heap: " + heapq.heappop(nums))
