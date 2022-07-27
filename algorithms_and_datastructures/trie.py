9991764747ba1967fd9e50962b0302d5265ba3f9
Claudio Rotolo <claudio.work69@gmail.com>
2022-07-27 19:44:15 +0000

    add: working basic trie print

--- algorithms_and_datastructures/trie.py
+++ algorithms_and_datastructures/trie.py
@@ -1,16 +1,18 @@
 from collections import defaultdict
 
+
 class Trie:
     """
     Implement a trie with insert, search, and startsWith methods.
     """
+
     def __init__(self):
         self.root = defaultdict()
 
     # @param {string} word
     # @return {void}
     # Inserts a word into the trie.
-    def insert(self, word):
+    def insert(self, word: str):
         current = self.root
         for letter in word:
             current = current.setdefault(letter, {})
@@ -19,7 +21,7 @@ class Trie:
     # @param {string} word
     # @return {boolean}
     # Returns if the word is in the trie.
-    def search(self, word):
+    def search(self, word: str):
         current = self.root
         for letter in word:
             if letter not in current:
@@ -33,7 +35,7 @@ class Trie:
     # @return {boolean}
     # Returns if there is any word in the trie
     # that starts with the given prefix.
-    def startsWith(self, prefix):
+    def startsWith(self, prefix: str):
         current = self.root
         for letter in prefix:
             if letter not in current:
@@ -41,20 +43,37 @@ class Trie:
             current = current[letter]
         return True
 
+    def __repr__(self):
+        def recur(node, indent):
+            if not node:
+                return ""
+            return "".join(
+                indent +
+                key +
+                recur(child, indent + "  ")
+                for key, child in node.items()
+            )
+
+        return recur(self.root, "\n")
+
+
 def create_trie(words: list[str]):
     head = Trie()
     for word in words:
         head.insert(word)
     return head
 
-trie = create_trie(["ciao", "ciaoo", "ciaopp", "bai"])
 
-print("\"ci\" in trie:", trie.search( "ci"))
-print("\"ciao\" in trie:", trie.search( "ciao"))
-print("\"ciaoo\" in trie:", trie.search( "ciaoo"))
-print("\"ciaop\" in trie:", trie.search( "ciaop"))
-print("\"cia\" in trie:", trie.search( "cia"))
-print("\"bai\" in trie:", trie.search( "bai"))
+trie = create_trie(["ciao", "ciaoo", "ciaopp", "bai"])
+print(trie)
+
+print("\"ci\" in trie:", trie.search("ci"))
+print("\"ciao\" in trie:", trie.search("ciao"))
+print("\"ciaoo\" in trie:", trie.search("ciaoo"))
+print("\"ciaop\" in trie:", trie.search("ciaop"))
+print("\"cia\" in trie:", trie.search("cia"))
+print("\"bai\" in trie:", trie.search("bai"))
 
 trie.insert("ci")
-print("\"ci\" in trie:", trie.search( "ci"))
+print(trie)
+print("\"ci\" in trie:", trie.search("ci"))
